4d0f2af9cf29964dfe6293adea4894c1ad0faae2
Thuan Tong <53972592+TIw1nner@users.noreply.github.com>
2021-11-19 10:06:52 +0700

    Update Bai17.cpp

--- Buoi01/Bai17.cpp
+++ Buoi01/Bai17.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main(){
     int gio,phut,giay;
-    cout << "Hay nhap so gio vao: ";
+    char h,p,s;
+    cout << "Hay nhap thoi gian vao: ";
     cin >> gio;
-    cout << "Hay nhap so phut vao: ";
+    cin >> h;
     cin >> phut;
-    cout << "Hay nhap so giay vao: ";
+    cin >> p;
     cin >> giay;
+    cin >> s;
     cout << "So giay la " << gio*3600 + phut *60 + giay;
     return 0;
 }
